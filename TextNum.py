3a600f1205966de446755cca555b87162d10700d
phancyn✨ <shvalinc@gmail.com>
2025-01-22 18:46:14 +0500

    Add files via upload

---
+++ TextNum.py
@@ -0,0 +1,15 @@
+﻿#Слова которые будут использоваться в изучении x y z 
+x = "Nice"
+y = "Bread"
+z = "Dick"
+
+
+w = (x + " " + z) 
+
+if w == "Nice" " " "Dick":
+    print("Great!")
+elif w != "Nice" " " "Dick":
+    print("Not!")
+
+
+print(w)
