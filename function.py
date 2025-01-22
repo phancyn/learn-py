3860c1607122c91a5ed90116a74a870a0e20db19
phancyn✨ <shvalinc@gmail.com>
2025-01-22 21:07:43 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,14 @@
+﻿def Low():
+    a = ("123")
+    b = (" ")
+    c = ("231")
+    print(a + b + c)
+
+def High():
+    a = ("321")
+    b = (" ")
+    c = ("421")
+    print(a + b + c)
+    
+Low()
+High()
